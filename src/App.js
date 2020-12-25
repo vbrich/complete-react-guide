ab87ffee414f3bfe871575f99df0791090dd012e
Rich Boesch <richard.boesch@gmail.com>
2020-12-24 22:15:12 -0800

    JSX fundamentals

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
+// import React from 'react'; 
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hi, I'm a React App... and this is a test</h1>
+      <h1>Hi, I'm a React App!</h1>
+      <p>This is really working...</p>
     </div>
+
+    // React is doing this with the JSX for us...
+    // - The code above is NOT HTML, but JSX
+    // - It is just doing this, but JSX helps remove the cumbersome effort of doing this
+    // React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Hi, I\'m a React App!'))
+
+    // JSX is Javascript, so we can't use certain keywords. "className" is JSX's variable to compile to "class". 
+    // Also, every <div>, etc is actually just the JSX name, but it really looks like HTML. 
+
+    // Normally, we want our JSX to return a single container element (e.g. <div>)
   );
 }
 
